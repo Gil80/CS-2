33d05b011e5f30fc8f4ce708d8b525d403caaae6
gil80 <just.gil@gmail.com>
2017-10-12 13:44:14 +1100

    add width.py

---
+++ width.py
@@ -0,0 +1,11 @@
+# Exercise 4: Assume that we execute the following assignment statements:
+width = 17
+height = 12.0
+exp1 = width//2
+exp2 = width/2.0
+exp3 = height/3
+exp4 = 1 + 2 * 5
+print("The value of expression 1 is:",exp1, "and its value type is:",type(exp1))
+print("The value of expression 2 is:",exp2, "and its value type is:",type(exp2))
+print("The value of expression 3 is:",exp3, "and its value type is:", type(exp3))
+print("The value of expression 4 is:",exp4, "and its value type is:",type(exp4))
